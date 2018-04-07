df039a6cb5a7a757e6684c94d04721af890aa2e7
Ryan Cheung <cheung31@gmail.com>
2018-04-07 15:59:12 -0700

    Move style declarations out of render method

--- src/history.js
+++ src/history.js
@@ -1,32 +1,30 @@
 import React from 'react';
 
-const History = ({ style, viewedAccount, currentPage, loadAccount, selectedNum, setSelected, setPage, loading }) => {
-
-  const transactionList = {
+const styles = {
+  transactionList: {
     listStyle: 'none',
     width: '500px',
     margin: 'auto',
-  }
-
-  const listBox = {
+  },
+  listBox: {
     padding: '5px',
     borderTop: '1px solid black',
     borderLeft: '1px solid black',
     borderRight: '1px solid black',
-  }
-
-  const closingLine = {
+  },
+  closingLine: {
     borderBottom: '1px solid black',
-  }
-
-  const cursor = {
+  },
+  cursor: {
     cursor: 'pointer',
-  }
-
-  const buttonContainer = {
+  },
+  buttonContainer: {
     display: 'flex',
     justifyContent: 'space-around',
   }
+};
+
+const History = ({ style, viewedAccount, currentPage, loadAccount, selectedNum, setSelected, setPage, loading }) => {
 
   return (
     loading ?
@@ -36,27 +34,27 @@ const History = ({ style, viewedAccount, currentPage, loadAccount, selectedNum, 
     :
       <div style={style}>
         <h2>{viewedAccount}</h2>
-        <ul style={transactionList}>
+        <ul style={styles.transactionList}>
           {       
             currentPage.map((transaction, i) => {
               return (
                 i < currentPage.length - 1 ?
-                  <li style={listBox}>
-                    To: <span style={cursor} onClick={(e)=>{loadAccount(e.target.innerText)}}>{transaction[0]}</span> <br />
-                    From:  <span style={cursor} onClick={(e)=>{loadAccount(e.target.innerText)}}>{transaction[1]}</span> <br />
+                  <li style={styles.listBox}>
+                    To: <span style={styles.cursor} onClick={(e)=>{loadAccount(e.target.innerText)}}>{transaction[0]}</span> <br />
+                    From:  <span style={styles.cursor} onClick={(e)=>{loadAccount(e.target.innerText)}}>{transaction[1]}</span> <br />
                     Value: {transaction[2]} <br />
                   </li>
                 :
-                  <li style={Object.assign({}, listBox, closingLine)}>
-                    To: <span style={cursor} onClick={(e)=>{loadAccount(e.target.innerText)}}>{transaction[0]}</span> <br />
-                    From:  <span style={cursor} onClick={(e)=>{loadAccount(e.target.innerText)}}>{transaction[1]}</span> <br />
+                  <li style={Object.assign({}, styles.listBox, styles.closingLine)}>
+                    To: <span style={styles.cursor} onClick={(e)=>{loadAccount(e.target.innerText)}}>{transaction[0]}</span> <br />
+                    From:  <span style={styles.cursor} onClick={(e)=>{loadAccount(e.target.innerText)}}>{transaction[1]}</span> <br />
                     Value: {transaction[2]} <br />
                   </li>
               )
             })
           }
         </ul>
-        <div style={buttonContainer}>
+        <div style={styles.buttonContainer}>
           <input value={selectedNum} onChange={setSelected}/>
           <button onClick={()=>{setPage(selectedNum)}}>Select Page</button>
         </div>
